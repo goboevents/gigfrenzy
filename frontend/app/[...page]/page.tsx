8c89f24b9f2e2811eb5a2ba8ac482b8abb7d2cdd
Ross Phillips <ross@Rosss-MBP-2.lan>
2025-08-18 21:22:48 -0500

    Clear all files from cursor-dev branch

--- frontend/app/[...page]/page.tsx
+++
@@ -1,7 +0,0 @@
-import DynamicPageClient from './DynamicPageClient';
-
-export default async function DynamicPage({ params }: { params: Promise<{ page: string[] }> }) {
-  const resolvedParams = await params;
-  console.log('DynamicPage params:', resolvedParams); // Should now log something like { page: ['test-page'] }
-  return <DynamicPageClient params={resolvedParams} />;
-}
